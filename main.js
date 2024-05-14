b3bc1a6aa83ac1669b6aaf5a9f273bead5136f70
Diyora31 <asilovad7>
2024-05-14 22:35:48 +0500

    done

---
+++ main.js
@@ -0,0 +1,88 @@
+// 1TASK
+
+let a=5%2
+alert(a)
+
+
+
+// 2TASK
+// let num = (Math.round(Math.random() * 10) )
+// alert(num) 
+
+
+
+// 3TASK
+// let b=12.510
+// console.log(Math.floor(b));
+
+
+
+
+// 5TASK
+
+// for (let i = 0; i <=9; i++) {
+//     console.log('Mars IT school');
+// }     
+
+
+
+
+// 7TASK
+// let  text =+prompt('Enter your age')
+
+// if(text<18){
+//     console.log('siz balogat yoshiga yetmagansiz');
+// }
+
+// else if(text==18){
+//     console.log('balogat yoshi muborak ');
+// }
+// else{
+//     console.log('Siz balagot yoshiga yetgansiz');
+// }
+
+
+
+// 8TASK
+
+//  let name1 = prompt('Ismingizni kiriting')
+
+// let resn = name1
+// .split('')
+// .reverse()
+// .join('')
+
+
+// console.log(resn);
+
+
+// 10TASK
+
+// let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i = 1; i <= num.length; i++) {
+//     if (i % 2 == 0) {
+//       console.log(`the even numbers:${i}`);
+//     }
+
+//   }
+
+
+// 6TASK
+
+// let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ]
+
+// I tried this task ,unfourtunetly I couldn't do it ,I will try to do it with my support or main teacher
+
+// 4TASK
+
+
+// function count(word) {
+//     return word.replace('').length
+// }
+
+// let word = 'Mars IT School'
+// let calculate = count(word)
+
+// alert(calculate)
+
